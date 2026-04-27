d8781bdc3f04eb2151df0d11214eaa171b6d0944
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-28 01:55:51 +0200

    feat(certificates): expose internal certificate review controller

--- src/modules/certificates/certificates.module.ts
+++ src/modules/certificates/certificates.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CertificatesController } from './certificates.controller';
+import { InternalCertificatesController } from './internal-certificates.controller';
 import { CertificatesService } from './certificates.service';
+import { AuthModule } from '../auth/auth.module';
 import { KeysModule } from '../keys/keys.module';
 
 @Module({
-  imports: [KeysModule],
-  controllers: [CertificatesController],
+  imports: [AuthModule, KeysModule],
+  controllers: [CertificatesController, InternalCertificatesController],
   providers: [CertificatesService],
   exports: [CertificatesService],
 })
